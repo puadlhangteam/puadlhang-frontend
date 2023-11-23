10c85d113d8addb361ff427f279992beb0cc7e01
Joespk <supakritjoe@gmail.com>
2023-11-23 18:09:28 +0700

    secret

---
+++ src/secret/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyANF8LZEJikuXXJgb_O2aBGZHDO9jCa6-s',
+  authDomain: 'first-5619d.firebaseapp.com',
+  projectId: 'first-5619d',
+  storageBucket: 'first-5619d.appspot.com',
+  messagingSenderId: '624645751168',
+  appId: '1:624645751168:web:abe7e0d3196c83d7a54165',
+  measurementId: 'G-YV2PCRS4XC',
+}
