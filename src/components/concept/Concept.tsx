bdc32c36cb0fd542f13adbfaa86b83839a8ac66f
Tonfern <thananaeam@gmail.com>
2023-11-27 02:24:15 +0700

    feathome-concept

--- src/components/concept/Concept.tsx
+++ src/components/concept/Concept.tsx
@@ -6,7 +6,17 @@ import { Link } from 'react-router-dom'
 const Concept = () => {
   return (
     <section id="content" className={classes.container}>
-      <motion.div className="mx-auto md:h-5/6">
+      <motion.div
+        className="mx-auto md:h-5/6"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ delay: 0.2, duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, x: 50 },
+          visible: { opacity: 1, x: 0 },
+        }}
+      >
         {/* GRAPHICS AND TEXT */}
         <div className="mt-6 items-center justify-between gap-20 md:mt-38 md:flex">
           <img className="mx-auto" alt="benefits-page-graphic" src={Joynow} />
@@ -24,7 +34,7 @@ const Concept = () => {
                 visible: { opacity: 1, x: 0 },
               }}
             >
-              <p className="my-5">
+              <p className="my-10" style={{ fontSize: '24px' }}>
                 แหล่งบทความเกี่ยวกับกล้ามเนื้อที่น่ารู้
                 ที่จะทำให้คุณสามารถป้องกันความเจ็บป่วยในปัจจุบันไม่ให้เกินขึ้นในอนาคต แต่ถ้าหากคุณต้องการ
                 การแก้ไขและท่ากายภาพอย่างตรงจุดเราแนะนำให้คุณ เลือกบริเวณได้ที่นี้
