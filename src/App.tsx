3d53595bf0a992f70cdb9eb93bdca7d4de37909e
bnnnnnogk <pakhin.nn1@gmail.com>
2023-11-27 13:52:45 +0700

    feat/navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import Login from './components/Login'
+import Navbar from "./components/Navbar"
+
 
 function App() {
-  return <Login />
+  return (
+    <>
+    <Navbar />
+    </>
+  )
 }
 
 export default App
