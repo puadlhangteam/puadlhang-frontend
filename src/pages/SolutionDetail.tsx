01b876c109be8b8f0c47f3d61543fc9df366519d
Joespk <supakritjoe@gmail.com>
2023-11-28 18:43:51 +0700

    usehook solutionDetailPage

--- src/pages/SolutionDetail.tsx
+++ src/pages/SolutionDetail.tsx
@@ -1,14 +1,20 @@
 import SolutionDetailPage from '../components/Solutions/SolutionDetailPage'
 import BannerSolutionDetailPage from '../components/Solutions/Banner/BannerSolutionDetailPage'
 import CommentsContainer from '../components/comments/CommentContainer'
+import useContentbysolutionId from '../hooks/useContentbysolutionId'
+import { useParams } from 'react-router-dom'
 
 function SolutionDetail() {
+  const { id } = useParams()
+  const { content } = useContentbysolutionId(id)
   return (
-    <div className="flex flex-col container mx-auto px-5 py-10">
-      <BannerSolutionDetailPage />
-      <SolutionDetailPage />
-      <CommentsContainer className="mt-28" logginedUserId="a" />
-    </div>
+    content && (
+      <div className="flex flex-col container mx-auto px-5 py-10">
+        <BannerSolutionDetailPage />
+        {<SolutionDetailPage key={content.solutionId} content={content} />}
+        <CommentsContainer className="mt-28" logginedUserId="a" />
+      </div>
+    )
   )
 }
 
