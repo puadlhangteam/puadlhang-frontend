c9b7b8f2e274bd91185e6f7741546132c72df25b
Joespk <supakritjoe@gmail.com>
2023-11-29 20:55:03 +0700

    add button Specilistform

--- src/components/Login/Profile.tsx
+++ src/components/Login/Profile.tsx
@@ -7,10 +7,16 @@ const Profile = () => {
 
   const navigate = useNavigate()
 
-  const handleSubmit = async (e: FormEvent) => {
+  const handleEditProfile = async (e: FormEvent) => {
     e.preventDefault()
     navigate('/login/editprofile')
   }
+
+  const handleEditSpecialistform = async (e: FormEvent) => {
+    e.preventDefault()
+    navigate('/login/specialist')
+  }
+
   if (!user) return
   return (
     <div className="flex-col justify-center items-center top-36 sm:right-24 md:right-44 lg:right-20 xl:right-40 2xl:right-64 w-4/5 mx-auto">
@@ -25,32 +31,35 @@ const Profile = () => {
             )}
           </div>
           <div className="flex justify-center">
-            <form className="grid grid-cols-3 text-[24px]">
+            <form className="grid grid-cols-2 gap-y-3 w-2/3 text-[24px]">
               <p className="col-start-1 font-bold">Name :</p>
-              {user.username && <p className="col-start-3"> {user.username}</p>}
+              {user.username && <p className="col-start-2"> {user.username}</p>}
               <p className="col-start-1 font-bold">Gender :</p>
-              {user.gender && <p className="col-start-3">{user.gender}</p>}
+              {user.gender && <p className="col-start-2">{user.gender}</p>}
               <p className="col-start-1 font-bold">Age :</p>
-              {user.gender && <p className="col-start-3">{user.age}</p>}
+              {user.gender && <p className="col-start-2">{user.age}</p>}
 
               {user.isSpecialist && (
                 <>
                   <p className="col-start-1">Role:</p>
-                  <p className="col-start-3">{user.isSpecialist}</p>
+                  <p className="col-start-2">{user.isSpecialist}</p>
                 </>
               )}
-            </form>
-          </div>
-          <div className="flex justify-center">
-            <div className="mt-12 text-center ">
               <button
                 type="submit"
-                onClick={handleSubmit}
-                className="w-48 bg-orange-600 text-white text-lg font-bold rounded-3xl  col-start-3 py-3 active:scale-[0.98] active:deration-75 hover:scale-[1.01] ease-in-out transition-all"
+                onClick={handleEditProfile}
+                className="w-48 bg-orange-600 text-white text-lg font-bold rounded-3xl  py-3 active:scale-[0.98] active:deration-75 hover:scale-[1.01] ease-in-out transition-all"
               >
                 Edit profile
               </button>
-            </div>
+              <button
+                type="submit"
+                onClick={handleEditSpecialistform}
+                className="w-48 bg-orange-600 text-white text-lg font-bold rounded-3xl  py-3 active:scale-[0.98] active:deration-75 hover:scale-[1.01] ease-in-out transition-all"
+              >
+                Specilistform
+              </button>
+            </form>
           </div>
         </form>
       </div>
