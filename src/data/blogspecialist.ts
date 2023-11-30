b47b606bcfea69a8c955fcadea40149197efaa87
Tonfern <thananaeam@gmail.com>
2023-11-30 16:04:10 +0700

    feathome-fix-profile

--- src/data/blogspecialist.ts
+++ src/data/blogspecialist.ts
@@ -3,33 +3,32 @@ import { BlogSpecialistDTO } from '../types/home'
 export const BLOGSPECIAL: BlogSpecialistDTO[] = [
   {
     thumbnailUrl:
-      'https://images.unsplash.com/photo-1559839734-2b71ea197ec2?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://scontent.fbkk22-3.fna.fbcdn.net/v/t1.6435-9/37161582_1923547757695583_5649034243771203584_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=7a1959&_nc_eui2=AeEqfvUs-TTUW5nXlr2K1KxGhI5jXp1iZ3CEjmNenWJncBoKVxGForw13WRdGex2Nnvlv6wYswRmYcxngMN3aBFn&_nc_ohc=Hh2L9HwqfgMAX-Ekzgd&_nc_ht=scontent.fbkk22-3.fna&oh=00_AfAsC4N9hLsrUNcR3mfFHoB40TgILe89-0I4rbZpSlOGKw&oe=658FB032',
     description: 'กล้ามเนื้อหัวใจมีเลือดไปเลี้ยงลดลงหรือไม่มีเลย เป็นผลให้การทำงานของกล้ามเนื้อหัวใจผิดปกติ ',
-    user: 'Dr. Jojosu',
+    user: 'Dr. A',
     date: 'หมอกล้ามเนื้อ กระดูกและข้อ',
   },
   {
     thumbnailUrl:
-      'https://images.unsplash.com/photo-1537368910025-700350fe46c7?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://scontent.fbkk22-1.fna.fbcdn.net/v/t1.18169-9/13241132_854417157996822_3503992854545489122_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=7a1959&_nc_eui2=AeFrSDGjFhXNV-gHnQHLJ3FPr_4e6w4zExCv_h7rDjMTEPkTm1uAihAmhHSezQWAr2Nt4OqobPTpInor6zFD_Eyr&_nc_ohc=qBZlof5A_eQAX9j-RF_&_nc_ht=scontent.fbkk22-1.fna&oh=00_AfCcHWpKm9cNg2XrTUrxoIWqkeVIIRQe2BjnMDHoYQlQMQ&oe=658FCCBF',
     description: 'ภาวะกล้ามเนื้อหัวใจขาดเลือดเฉียบพลันพบได้กับผู้ป่วยที่สูบบุหรี่ไฟฟ้ามาเป็นเวลานาน',
     user: 'Dr. Bunny',
     date: 'หมอกล้ามเนื้อ กระดูกและข้อ',
   },
 
   {
-    thumbnailUrl:
-      'https://images.unsplash.com/photo-1657551760830-8a90773fa68c?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    thumbnailUrl: 'https://avatars.githubusercontent.com/u/37364131?v=4',
     description:
       'อยากให้ทุกคนดูแลร่างกายตัวเอง และกายภาพตัวเองอยู่เสมอในช่วงเวลาที่ทำงานอยู่เว็บไซต์ของเราจะช่วยคุณเอง',
-    user: 'Dr. Tommy',
+    user: 'Dr. Tiger',
     date: 'หมอกระดูกและเส้นประสาท',
   },
   {
     thumbnailUrl:
-      'https://images.unsplash.com/photo-1622902046580-2b47f47f5471?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://scontent.fbkk22-8.fna.fbcdn.net/v/t39.30808-6/313064137_3351820978477183_907762994162051876_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeFtJJCsQmBRskWK22qEqIrmIri9i-WwrOQiuL2L5bCs5GpBgfC9BHflnaFWngDEaS2g5gnXoEg_YZmWvcxmkch3&_nc_ohc=Ikvwlo45VUwAX9lNEz4&_nc_ht=scontent.fbkk22-8.fna&oh=00_AfBjzw-dY1UeDIbYx30fjbcOEFJZ8wGLr_Yo18UghOz8BQ&oe=656CA9E7',
     description:
       'อยากให้ทุกคนดูแลร่างกายตัวเอง และกายภาพตัวเองอยู่เสมอในช่วงเวลาที่ทำงานอยู่เว็บไซต์ของเราจะช่วยคุณเอง',
-    user: 'Dr. Wannawong',
+    user: 'Dr.Peet',
     date: 'นักกายภาพบำบัด',
   },
 ]
--- src/data/review.ts
+++ src/data/review.ts
@@ -3,31 +3,31 @@ import { ReviewsDTO } from '../types/home'
 export const REVIEWS: ReviewsDTO[] = [
   {
     thumbnailUrl:
-      'https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'เป็นเว็บที่ใช้งานง่าย และได้รับการแก้ไขปัญหาที่ตรงจุด',
-    username: 'คุณธนกชนัน',
-    rating: 5,
+      'https://scontent.fbkk22-8.fna.fbcdn.net/v/t1.18169-9/17264857_1565916033437753_4142327870996366864_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=7a1959&_nc_eui2=AeHkNCFH1IVTr4GT-M67F1d2SdkG9DvP2e9J2Qb0O8_Z7-Gx6eZBNQJqrbv7f5lZeHJlq6X960h40kb1Dfg5oQ40&_nc_ohc=SIJPECX3hhIAX90lNcQ&_nc_ht=scontent.fbkk22-8.fna&oh=00_AfC9nffOjGN2vSgxdVD_KQv-KdIRMcS7eP2b5O_I0ikqmQ&oe=658FC643',
+    description: 'ผมทำโปรเจคแล้วปวดกล้ามเนื้อหัวมากฮะ เพราะผมเป็น PM ของกลุ่ม ทำเสร็จแล้วใช้งานต่อเลยครับ',
+    username: 'Banana',
+    github: 'https://github.com/bunnanna',
   },
   {
     thumbnailUrl:
-      'https://images.unsplash.com/photo-1624526691205-0e2870f30702?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'วิธีการกายภาพนี้ดีมากครับ ผมทำแล้วอาการดีขึ้น ถือว่าช่วยได้เยอะเลย',
-    username: ' คุณวีระ',
-    rating: 4,
+      'https://scontent.fbkk22-6.fna.fbcdn.net/v/t1.6435-9/87254886_3053391194705990_4866519463324811264_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=be3454&_nc_eui2=AeFiAo5rtV5xYp3i2Hc0whyVJ2_vLzIs2wEnb-8vMizbAR9lS1T1lSO_YDT2YBss9FD2Fp0vKV564l0Hunev-_BO&_nc_ohc=N2vSS8-GIPYAX9rpbyK&_nc_ht=scontent.fbkk22-6.fna&oh=00_AfAUG7Ffz-ZCn0qIV4v1MzQHTcqdkodJjckDpItxrxbQMw&oe=658FB94A',
+    description: 'ในขณะที่ทำงาน ผมปวดหลังมากเลยฮะ ผมทำ logic ของ front-end ไม่มีเวลาไปหาหมอ เว็บนี้ช่วยได้มากเลยครับ',
+    username: 'Jojo',
+    github: 'https://github.com/Joespk',
   },
 
   {
     thumbnailUrl:
-      'https://images.unsplash.com/photo-1515994034738-1f437c226687?q=80&w=1984&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'ไม่ค่อยได้มีเวลาไปหาหมอเลยครับ แต่วิธีนี้ เป็นวิธีที่สามารถทำให้แก้ปัญหาได้บ้าง',
-    username: 'คุณทอม',
-    rating: 5,
+      'https://scontent.fbkk22-2.fna.fbcdn.net/v/t39.30808-6/324901446_1214985249124012_7482119672844670913_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=9c7eae&_nc_eui2=AeHMh3vA_nJgeVWS4f9dWQdJra8dFEQlirGtrx0URCWKscjsKDaitXiEI2CuyLrHzu7AXE8s61g9F4CS9yIwP4ZZ&_nc_ohc=lL7WbzQ-orcAX-KHh_W&_nc_ht=scontent.fbkk22-2.fna&oh=00_AfBT5m0SD8oc9mZSIHnsg-jGESxhMyo3yxpYLK60f4NDOQ&oe=656C4415',
+    description:
+      'ปัญหาคือการจัดการเวลาในการดูแลตัวเอง เพราะเป็น front-end เว็บนี้สามารถช่วยให้หมดกังวลเรื่องต้องไปพบแพทย์ได้เบื้อต้นเลย',
+    username: 'Dontgiveup',
+    github: 'https://github.com/DONTGIVEUPTOTHEMOON',
   },
   {
-    thumbnailUrl:
-      'https://images.unsplash.com/photo-1488751045188-3c55bbf9a3fa?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: "'วิธีการที่บอก สามารถเข้าใจได้ง่ายดีค่ะ มีผู้เชียวชาญมาแนะนำต่อด้วย'",
-    username: 'คุณกนกวรรณ',
-    rating: 4,
+    thumbnailUrl: 'https://avatars.githubusercontent.com/u/116068149?v=4',
+    description: 'ผมปวดหลังแต่เป็นโควิทด้วยฮะเลยต้องนั้งทำ front-end อยากบรรเทาอาการหลังเว็บนี้ช่วยได้เยอะเลยฮะ',
+    username: 'K.kong',
+    github: 'https://github.com/bnnnnnogk',
   },
 ]
